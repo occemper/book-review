e443cf6cf7c5f41f0bd9748afdc68a4a35474e29
occemper <ravilre22@gmail.com>
2025-04-07 16:24:33 +0300

    008 Aggregations on Relations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,20 +33,11 @@ class DatabaseSeeder extends Seeder
                 ->create();
         });
 
-        Book::factory(33)->create()->each(function ($book) {
+        Book::factory(34)->create()->each(function ($book) {
             $numReviews = random_int(5, 30);
 
             Review::factory()->count($numReviews)
                 ->bad()
-                ->for($book)
-                ->create();
-        });
-
-        Book::factory(11)->create()->each(function ($book) {
-            $numReviews = random_int(5, 30);
-
-            Review::factory()->count($numReviews)
-                ->good()
                 ->for($book)
                 ->create();
         });
